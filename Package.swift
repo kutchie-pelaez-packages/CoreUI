13f9923e230fa97874bdbf8069fe3073c6b4d813
ivan <kulikovia.72@icloud.com>
2022-01-28 04:50:17 +0200

    update package manifest

--- Package.swift
+++ Package.swift
@@ -8,7 +8,12 @@ let package = Package(
         .iOS("15")
     ],
     products: [
-        .library(name: "CoreUI", targets: ["CoreUI"])
+        .library(
+            name: "CoreUI",
+            targets: [
+                "CoreUI"
+            ]
+        )
     ],
     dependencies: [
         .package(name: "Core", url: "https://github.com/kutchie-pelaez-packages/Core.git", .branch("master"))
