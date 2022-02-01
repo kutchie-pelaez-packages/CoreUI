6bae7cb01d8b7ea3fca1289f4ab58dc720736b33
ivan <kulikovia.72@icloud.com>
2022-02-01 15:49:34 +0200

    add `dynamic` fonts

--- Sources/Misc/SystemFonts.swift
+++ Sources/Misc/SystemFonts.swift
@@ -2,6 +2,28 @@ import UIKit
 
 public enum SystemFonts {
     public enum Mono {
+        public static var largeTitle: UIFont { regular(34) }
+
+        public static var title1: UIFont { regular(28) }
+
+        public static var title2: UIFont { regular(22) }
+
+        public static var title3: UIFont { regular(20) }
+
+        public static var headline: UIFont { semibold(17) }
+
+        public static var body: UIFont { regular(17) }
+
+        public static var callout: UIFont { regular(16) }
+
+        public static var subheadline: UIFont { regular(15) }
+
+        public static var footnote: UIFont { regular(13) }
+
+        public static var caption1: UIFont { regular(12) }
+
+        public static var caption2: UIFont { regular(11) }
+
         public static func ultraLight(_ size: Double) -> UIFont {
             .monospacedSystemFont(
                 ofSize: size,
@@ -65,6 +87,28 @@ public enum SystemFonts {
             )
         }
     }
+
+    public static var largeTitle: UIFont { regular(34) }
+
+    public static var title1: UIFont { regular(28) }
+
+    public static var title2: UIFont { regular(22) }
+
+    public static var title3: UIFont { regular(20) }
+
+    public static var headline: UIFont { semibold(17) }
+
+    public static var body: UIFont { regular(17) }
+
+    public static var callout: UIFont { regular(16) }
+
+    public static var subheadline: UIFont { regular(15) }
+
+    public static var footnote: UIFont { regular(13) }
+
+    public static var caption1: UIFont { regular(12) }
+
+    public static var caption2: UIFont { regular(11) }
 
     public static func ultraLight(_ size: Double) -> UIFont {
         .systemFont(
