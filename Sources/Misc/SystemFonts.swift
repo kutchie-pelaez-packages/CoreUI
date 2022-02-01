3e68bdce3b9f458ef8ad3f09130b22b98933e9ce
ivan <kulikovia.72@icloud.com>
2022-02-01 15:38:04 +0200

    remove dynamic & add mono system fonts

--- Sources/Misc/SystemFonts.swift
+++ Sources/Misc/SystemFonts.swift
@@ -1,72 +1,131 @@
 import UIKit
 
 public enum SystemFonts {
-    public static func dynamic(for style: UIFont.TextStyle) -> UIFont {
-        UIFont.preferredFont(forTextStyle: style)
-    }
-
-    public enum Static {
+    public enum Mono {
         public static func ultraLight(_ size: Double) -> UIFont {
-            .systemFont(
+            .monospacedSystemFont(
                 ofSize: size,
                 weight: .ultraLight
             )
         }
 
         public static func thin(_ size: Double) -> UIFont {
-            .systemFont(
+            .monospacedSystemFont(
                 ofSize: size,
                 weight: .thin
             )
         }
 
         public static func light(_ size: Double) -> UIFont {
-            .systemFont(
+            .monospacedSystemFont(
                 ofSize: size,
                 weight: .light
             )
         }
 
         public static func regular(_ size: Double) -> UIFont {
-            .systemFont(
+            .monospacedSystemFont(
                 ofSize: size,
                 weight: .regular
             )
         }
 
         public static func medium(_ size: Double) -> UIFont {
-            .systemFont(
+            .monospacedSystemFont(
                 ofSize: size,
                 weight: .medium
             )
         }
 
         public static func semibold(_ size: Double) -> UIFont {
-            .systemFont(
+            .monospacedSystemFont(
                 ofSize: size,
                 weight: .semibold
             )
         }
 
         public static func bold(_ size: Double) -> UIFont {
-            .systemFont(
+            .monospacedSystemFont(
                 ofSize: size,
                 weight: .bold
             )
         }
 
         public static func heavy(_ size: Double) -> UIFont {
-            .systemFont(
+            .monospacedSystemFont(
                 ofSize: size,
                 weight: .heavy
             )
         }
 
         public static func black(_ size: Double) -> UIFont {
-            .systemFont(
+            .monospacedSystemFont(
                 ofSize: size,
                 weight: .black
             )
         }
+    }
+
+    public static func ultraLight(_ size: Double) -> UIFont {
+        .systemFont(
+            ofSize: size,
+            weight: .ultraLight
+        )
+    }
+
+    public static func thin(_ size: Double) -> UIFont {
+        .systemFont(
+            ofSize: size,
+            weight: .thin
+        )
+    }
+
+    public static func light(_ size: Double) -> UIFont {
+        .systemFont(
+            ofSize: size,
+            weight: .light
+        )
+    }
+
+    public static func regular(_ size: Double) -> UIFont {
+        .systemFont(
+            ofSize: size,
+            weight: .regular
+        )
+    }
+
+    public static func medium(_ size: Double) -> UIFont {
+        .systemFont(
+            ofSize: size,
+            weight: .medium
+        )
+    }
+
+    public static func semibold(_ size: Double) -> UIFont {
+        .systemFont(
+            ofSize: size,
+            weight: .semibold
+        )
+    }
+
+    public static func bold(_ size: Double) -> UIFont {
+        .systemFont(
+            ofSize: size,
+            weight: .bold
+        )
+    }
+
+    public static func heavy(_ size: Double) -> UIFont {
+        .systemFont(
+            ofSize: size,
+            weight: .heavy
+        )
+    }
+
+    public static func black(_ size: Double) -> UIFont {
+        .systemFont(
+            ofSize: size,
+            weight: .black
+        )
     }
 }
